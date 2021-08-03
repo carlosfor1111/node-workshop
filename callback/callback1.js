939c3b66f0fcfaada16a14c36c2dd8c494d3e4a4
carlosfor1111 <qw5980165@gmail.com>
2021-08-03 10:41:22 +0800

    revise content mistake

--- callback/callback1.js
+++ callback/callback1.js
@@ -1,38 +1,38 @@
-// let doWork = function(job , timer , cb){
-//     setTimeout(()=>{
-//         let dt = new Date();
-//         cb(null, `完成工作: ${job} at ${dt.toISOString()}` );
-//     },timer);
-// };
+let doWork = function(job , timer , cb){
+    setTimeout(()=>{
+        let dt = new Date();
+        cb(null, `完成工作: ${job} at ${dt.toISOString()}` );
+    },timer);
+};
 
-// //callback hell
+//callback hell
 
-// let dt = new Date();
-// console.log(`開始工作 at ${dt.toISOString()}`);
+let dt = new Date();
+console.log(`開始工作 at ${dt.toISOString()}`);
 
-// doWork("刷牙" ,3000, function(err, data){
-//     if(err){
-//         console.err('發生錯誤了:',err);
-//         return;
-//     }
-//     console.log(data);
+doWork("刷牙" ,3000, function(err, data){
+    if(err){
+        console.err('發生錯誤了:',err);
+        return;
+    }
+    console.log(data);
 
-//     doWork("吃早餐", 3000, function (err, data) {
-//       if (err) {
-//         console.err("發生錯誤了:", err);
-//         return;
-//       }
-//       console.log(data);
+    doWork("吃早餐", 3000, function (err, data) {
+      if (err) {
+        console.err("發生錯誤了:", err);
+        return;
+      }
+      console.log(data);
 
-//         doWork("睡覺", 5000, function (err, data) {
-//           if (err) {
-//             console.err("發生錯誤了:", err);
-//             return;
-//           }
-//           console.log(data);
-//         });
-//     });
-// });
+        doWork("睡覺", 5000, function (err, data) {
+          if (err) {
+            console.err("發生錯誤了:", err);
+            return;
+          }
+          console.log(data);
+        });
+    });
+});
 
 // /* 使用foreach寫法 */
 
