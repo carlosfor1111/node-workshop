e15515a850e6547eb3411027eeaa95b38a4cd4d1
carlosfor1111 <qw5980165@gmail.com>
2021-08-07 10:01:21 +0800

    revise quiz1 & quiz2

--- promise/quiz2.js
+++ promise/quiz2.js
@@ -13,4 +13,7 @@ console.log(4);
 asyncF();
 console.log(5);
 
-//4第一個被呼叫先執行,然後是呼叫async()裡面的1，然後是呼叫5(2.3因為async await 會一起等待到其他執行完才執行)
+// 4->1->5->2->3
+
+//4第一個被呼叫先執行,然後是呼叫async()裡面的1，然後是呼叫5 
+// (2.3因為async await 被放到Q排隊會一起等待到其他執行完才能被EVENT LOOP拿回STACK執行)
