7fe1f52063b056bdcb2db7c2282ec58f95cccb24
carlosfor1111 <qw5980165@gmail.com>
2021-08-07 11:15:08 +0800

    revise await and xhr-await

--- promise/await.js
+++ promise/await.js
@@ -1,27 +1,31 @@
 //await //async
 // 基於promise 語法糖
+let doWork = function (job, timer, isOK) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let dt = new Date();
+      if (isOK) {
+        resolve(`完成工作: ${job} at ${dt.toISOString()}`);
+      } else {
+        reject(`失敗了${job}`);
+      }
+    }, timer);
+  });
+};
+
 async function doAllWorks() {
-  let doWork = function (job, timer, isOK) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        let dt = new Date();
-        if (isOK) {
-          resolve(`完成工作: ${job} at ${dt.toISOString()}`);
-        } else {
-          reject(`失敗了${job}`);
-        }
-      }, timer);
-    });
-  };
+  try {
+    let result1 = await doWork("刷牙", 3000, true);
+    console.log(result1);
 
-  let result1 = await doWork("刷牙", 3000, true);
-  console.log(result1);
+    let result2 = await doWork("吃早餐", 5000, true);
+    console.log(result2);
 
-  let result2 = await doWork("吃早餐", 5000, true);
-  console.log(result2);
-
-  let result3 = await doWork("睡覺", 3000, true);
-  console.log(result3);
+    let result3 = await doWork("睡覺", 3000, true);
+    console.log(result3);
+    
+  } catch (error) {
+    console.log(error);
+  }
 }
 doAllWorks();
-
