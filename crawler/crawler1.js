cdc4369d9d63523db00d6162bd4308433b152624
carlosfor1111 <qw5980165@gmail.com>
2021-08-07 16:02:08 +0800

    add moment npm and stock web crawler

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -1,3 +1,21 @@
+// https://www.twse.com.tw/zh/page/trading/exchange/STOCK_DAY.html
 
+
+const axios = require("axios")
+const moment = require("moment");
 
 axios
+  .get(
+    "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+    {
+      params: {
+        response: "json",
+        date: moment().format("YYYYMMDD"),
+        stockNo: "2330",
+      },
+    }
+  )
+
+  .then((response) => {
+    console.log(response.data);
+  });
