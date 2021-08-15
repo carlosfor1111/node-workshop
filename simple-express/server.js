a28123ab5ebabc3905635560cba442ac09c3ba24
carlosfor1111 <87885187+carlosfor1111@users.noreply.github.com>
2021-08-15 11:23:36 +0800

    add middelware function

--- simple-express/server.js
+++ simple-express/server.js
@@ -4,12 +4,24 @@ const express = require('express')
 
 const app = express()
 
+// app.use使用一個中間件
+// app.use(handler)
+app.use((req, res, next) => {
+  let current = new Date();
+  console.log(`有人訪問 at ${current.toISOString()}`)
+  next()
+})
+app.use((req, res, next) => {
+  console.log('我是第二個中間件')
+  next()
+})
 // HTTP Method :get,post,put,patch ,delete
+// router 路由
 app.get('/', function (request, response, next) {
   response.send('Hello')
 })
 
-app.get('/', function (request, response, next) {
+app.get('/about', function (request, response, next) {
   response.send('about')
 })
 
