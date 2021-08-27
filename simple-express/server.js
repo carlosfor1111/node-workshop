59baa905e85f51801a19a1a2fde3ea739ec36277
carlosfor1111 <87885187+carlosfor1111@users.noreply.github.com>
2021-08-27 13:24:58 +0800

    upload express homwork1

--- simple-express/server.js
+++ simple-express/server.js
@@ -1,13 +1,16 @@
+const cors = require('cors')
 const express = require('express')
+const connection = require('./utils/db')
 
 // 利用express 建立一個express application
 
 const app = express()
 
-// app.use使用一個中間件
-// app.use(handler)
+app.use(cors())
+
+// app.use使用一個中間件nom
 app.use((req, res, next) => {
-  let current = new Date();
+  const current = new Date()
   console.log(`有人訪問 at ${current.toISOString()}`)
   next()
 })
@@ -24,7 +27,25 @@ app.get('/', function (request, response, next) {
 app.get('/about', function (request, response, next) {
   response.send('about')
 })
+app.get('/stock', async (req, res, next) => {
+  const result = await connection.queryAsync('SELECT * FROM stock')
+  res.json(result)
+})
 
-app.listen(3000, function () {
+app.get('/stock/:stockCode', async (req, res, next) => {
+  const result = await connection.queryAsync(
+    'SELECT * FROM stock_price WHERE stock_id=?',
+    [req.params.stockCode]
+  )
+  res.json(result)
+})
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'NOT FOUND' })
+})
+
+app.listen(3000, async function () {
+  // 因為改成pool,不用手動連線
+  // await connection.connectAsync();
   console.log('我們的webserver啟動了')
 })
